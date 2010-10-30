4143471126c20a6c9f4d46c896beafbbfd18caa0
R.I.Pienaar <rip@devco.net>
2010-10-30 16:37:48 +0100

    Add some stuff thats useful but not exposed via system properties

--- plugins/mcollective/facts/jvm.rb
+++ plugins/mcollective/facts/jvm.rb
@@ -7,6 +7,7 @@ module MCollective
         # facts
         class Jvm<Base
             include_class java.lang.System
+            import java.lang.management.ManagementFactory
 
             def get_facts
                 facts = {}
@@ -14,6 +15,17 @@ module MCollective
                 System.get_properties.to_a.each do |prop|
                     facts[prop[0]] = prop[1]
                 end
+
+                runtime_bean = ManagementFactory.getRuntimeMXBean
+                facts["java.vm.uptime"] = runtime_bean.getUptime / 1000
+
+                classloading_bean = ManagementFactory.getClassLoadingMXBean
+                facts["java.vm.loaded_classes"] = classloading_bean.getLoadedClassCount
+                facts["java.vm.total_loaded_classes"] = classloading_bean.getTotalLoadedClassCount
+                facts["java.vm.unloaded_classes"] = classloading_bean.getUnloadedClassCount
+
+                os_bean = ManagementFactory.getOperatingSystemMXBean
+                facts["os.available_processors"] = os_bean.getAvailableProcessors
 
                 facts
             end
