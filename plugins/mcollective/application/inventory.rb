91528864a8e19954a4d116c5194e568f651239ed
R.I.Pienaar <rip@devco.net>
2011-01-17 17:37:11 +0000

    5897 - Improve client applications
    convert mc-inventory to new format and make it more robust

--- plugins/mcollective/application/inventory.rb
+++ plugins/mcollective/application/inventory.rb
@@ -99,7 +99,147 @@ class MCollective::Application::Inventory<MCollective::Application
         util.disconnect
     end
 
+    # Helpers to create a simple DSL for scriptlets
+    def format(fmt)
+        @fmt = fmt
+    end
+
+    def fields(&blk)
+        @flds = blk
+    end
+
+    def identity
+        @node[:identity]
+    end
+
+    def facts
+        @node[:facts]
+    end
+
+    def classes
+        @node[:classes]
+    end
+
+    def agents
+        @node[:agents]
+    end
+
+    def page_length(len)
+        @page_length = len
+    end
+
+    def page_heading(fmt)
+        @page_heading = fmt
+    end
+
+    def page_body(fmt)
+        @page_body = fmt
+    end
+
+    # Expects a simple printf style format and apply it to
+    # each node:
+    #
+    #    inventory do
+    #        format "%s:\t\t%s\t\t%s"
+    #
+    #        fields { [ identity, facts["serialnumber"], facts["productname"] ] }
+    #    end
+    def inventory(&blk)
+        raise "Need to give a block to inventory" unless block_given?
+
+        blk.call if block_given?
+
+        raise "Need to define a format" if @fmt.nil?
+        raise "Need to define inventory fields" if @flds.nil?
+
+        util = rpcclient("rpcutil", :options => @options)
+        util.progress = false
+
+        util.inventory do |t, resp|
+            @node = {:identity => resp[:sender],
+                     :facts    => resp[:data][:facts],
+                     :classes  => resp[:data][:classes],
+                     :agents   => resp[:data][:agents]}
+
+            puts @fmt % @flds.call
+        end
+
+        util.disconnect
+    end
+
+    # Use the ruby formatr gem to build reports using Perls formats
+    #
+    # It is kind of ugly but brings a lot of flexibility in report
+    # writing without building an entire reporting language.
+    #
+    # You need to have formatr installed to enable reports like:
+    #
+    #    formatted_inventory do
+    #        page_length 20
+    #
+    #        page_heading <<TOP
+    #
+    #                Node Report @<<<<<<<<<<<<<<<<<<<<<<<<<
+    #                            time
+    #
+    #    Hostname:         Customer:     Distribution:
+    #    -------------------------------------------------------------------------
+    #    TOP
+    #
+    #        page_body <<BODY
+    #
+    #    @<<<<<<<<<<<<<<<< @<<<<<<<<<<<< @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    #    identity,    facts["customer"], facts["lsbdistdescription"]
+    #                                    @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    #                                    facts["processor0"]
+    #    BODY
+    #    end
+    def formatted_inventory(&blk)
+        require 'formatr'
+
+        raise "Need to give a block to formatted_inventory" unless block_given?
+
+        blk.call if block_given?
+
+        raise "Need to define page body format" if @page_body.nil?
+
+        body_fmt = FormatR::Format.new(@page_heading, @page_body)
+        body_fmt.setPageLength(@page_length)
+        time = Time.now
+
+        util = rpcclient("rpcutil", :options => @options)
+        util.progress = false
+
+        util.inventory do |t, resp|
+            @node = {:identity => resp[:sender],
+                     :facts    => resp[:data][:facts],
+                     :classes  => resp[:data][:classes],
+                     :agents   => resp[:data][:agents]}
+
+            body_fmt.printFormat(binding)
+        end
+
+        util.disconnect
+    rescue Exception => e
+        STDERR.puts "Could not create report: #{e.class}: #{e}"
+        exit 1
+    end
+
+    @fmt = nil
+    @flds = nil
+    @page_heading = nil
+    @page_body = nil
+    @page_length = 40
+
     def main
-        node_inventory
+        if configuration[:script]
+            if File.exist?(configuration[:script])
+                eval(File.read(configuration[:script]))
+            else
+                raise "Could not find script to run: #{configuration[:script]}"
+            end
+        else
+            node_inventory
+        end
     end
 end
